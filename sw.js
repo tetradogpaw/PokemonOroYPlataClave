aca2f6b491822aaee012a8bee50861cce57bee8e
gabriel <tetradog@gmail.com>
2019-10-14 01:44:50 +0200

    commit

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 const CACHE_VERSION = 6;
 const CACHE_INMUTABLE = "CACHE_INMUTABLE";
-const INMUTABLES = ["index.html", "icon.png", "manifest.json"];
+const INMUTABLES = ["/index.html", "/icon.png", "/manifest.json"];
 
 
 self.addEventListener('install', e => {
 
     e.waitUntil(caches.open(CACHE_INMUTABLE)
         .then(cache => {
-            return cache.addAll(INMUTABLES).then(() => { cache.put("/", cache[INMUTABLES[0]]); });
+            return cache.addAll(INMUTABLES);
 
         }));
 
@@ -16,13 +16,6 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
 
-    var url;
-    if (String(e.request.url).includes("PokemonOroYPlataClave"))
-        url = String(e.request.url).substr("https://tetradogpaw.github.io".length);
-    else url = e.request.url;
-
-    console.log(url);
-
-    e.respondWith(caches.match(url));
+    e.respondWith(caches.match(e.request));
 
 });
