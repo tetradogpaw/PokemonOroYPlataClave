e4d39007b8367766f1b76a6222da42248e1c1cca
gabriel <tetradog@gmail.com>
2019-10-13 10:56:31 +0200

    commit

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_VERSION = 3;
 const CACHE_INMUTABLE = "CACHE_INMUTABLE";
-const INMUTABLES = ["/", "index.html"];
+const INMUTABLES = ["index.html"];
 
 
 self.addEventListener('install', e => {
