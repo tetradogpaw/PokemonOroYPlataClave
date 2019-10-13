c125b6c45679c39c8e760502ea643cc2f3cfd481
gabriel <tetradog@gmail.com>
2019-10-14 01:53:48 +0200

    commit

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const CACHE_VERSION = 6;
 const CACHE_INMUTABLE = "CACHE_INMUTABLE";
-const INMUTABLES = ["/index.html", "/icon.png", "/manifest.json"];
+const CACHE_DINAMICO = "CACHE_DINAMICO";
+const INMUTABLES = [];
 
 
 self.addEventListener('install', e => {
@@ -16,6 +17,18 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', e => {
 
-    e.respondWith(caches.match(e.request));
+    e.respondWith(caches.match(e.request).then(resp => {
+        var respuesta;
+        if (resp)
+            respuesta = resp;
+        else {
+            respuesta = fetch(e.request);
+            caches.open(CACHE_DINAMICO).then(cache => {
+                cache.put(e.request, respuesta.clone());
+            });
+        }
+        return respuesta;
+
+    }));
 
 });
