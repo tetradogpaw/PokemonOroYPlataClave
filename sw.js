3c3f0049dc4d60506531cbbfb0ef36e752bbeb2c
gabriel <tetradog@gmail.com>
2019-10-14 03:44:19 +0200

    commit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 7.6;
+const CACHE_VERSION = 7.7;
 const CACHE_INMUTABLE = "CACHE_INMUTABLE";
 const CACHE_DINAMICO = "CACHE_DINAMICO";
 const INMUTABLES = [];
@@ -6,11 +6,22 @@ const INMUTABLES = [];
 
 self.addEventListener('install', e => {
 
-    e.waitUntil(caches.open(CACHE_INMUTABLE)
+    var inmutable = caches.open(CACHE_INMUTABLE)
         .then(cache => {
+
             return cache.addAll(INMUTABLES);
 
-        }));
+        });
+    var dinamico = new Primise((okey, error) => {
+        caches.open(CACHE_DINAMICO).then(cache => {
+            cache.keys.forEach(key => {
+                cache.delete(key);
+
+            });
+            okey();
+        });
+    });
+    Promise.all(inmutable, dinamico);
 
 });
 
